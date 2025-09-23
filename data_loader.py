8ae9f442db4117f441141d121b05f841ec8f6ea1
Екатерина Лавриненко <lawr2013@gmail.com>
2025-09-23 10:32:05 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-FILE_ID = "19T-Gm5Dovnae7htHjJP2gUt3Tyl6NPp2"  # ID файла на Google Drive
-file_url = https://drive.google.com/file/d/19T-Gm5Dovnae7htHjJP2gUt3Tyl6NPp2/viewid={FILe_ID}"
+file_id = "19T-Gm5Dovnae7htHjJP2gUt3Tyl6NPp2"  # ID файла на Google Drive
+file_url = f"https://drive.google.com/uc?id={file_id}"
 
 raw_data = pd.read_csv(file_url)     # читаем файл
+
 
 raw_data.head(10)

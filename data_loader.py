39fb6ef1d9dc1041d958f0d01da045d3daa355bb
Екатерина Лавриненко <lawr2013@gmail.com>
2025-09-23 10:29:38 +0300

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+FILE_ID = "19T-Gm5Dovnae7htHjJP2gUt3Tyl6NPp2"  # ID файла на Google Drive
+file_url = https://drive.google.com/file/d/19T-Gm5Dovnae7htHjJP2gUt3Tyl6NPp2/viewid={FILe_ID}"
+
+raw_data = pd.read_csv(file_url)     # читаем файл
+
+raw_data.head(10)
